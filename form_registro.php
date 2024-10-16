37c9bbc6d930281f449bfd28926f743b9ccb564b
hadbet <hadbet.altamirano@grammer.com>
2024-10-16 16:56:30 -0600

    CargaInicial

--- form_registro.php
+++ form_registro.php
@@ -121,9 +121,12 @@
                             <table id="tablaCursosPendientes" class="table table-hover">
                                 <thead>
                                 <tr>
-                                    <th>Nombre Curso</th>
-                                    <th>Fecha</th>
-                                    <th>Horario</th>
+                                    <th>Id</th>
+                                    <th>Folio</th>
+                                    <th>Documento</th>
+                                    <th>Fecha Peticion</th>
+                                    <th>Usuario Peticion</th>
+                                    <th>Estatus</th>
                                 </tr>
                                 </thead>
                                 <tbody>
@@ -154,318 +157,10 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-56159088-1"></script>
 <script>
-
-    var radios = document.getElementsByName('inlineRadioOptions');
-
-    // Recorre todos los elementos
-    for(var i = 0; i < radios.length; i++){
-        radios[i].addEventListener('click', function(){
-            if(this.value == "Administrativo"){
-                var divApu = document.getElementById("divApu");
-                divApu.style.display = 'none';
-                divApu.classList.add('animate__animated', 'animate__bounceIn');
-
-                var divSupervisor = document.getElementById("divSupervisor");
-                divSupervisor.style.display = 'none';
-                divSupervisor.classList.add('animate__animated', 'animate__bounceIn');
-
-                var divShiftleader = document.getElementById("divShiftleader");
-                divShiftleader.style.display = 'none';
-                divShiftleader.classList.add('animate__animated', 'animate__bounceIn');
-
-                var divSuper = document.getElementById("divEncargado");
-                divSuper.style.display = 'block';
-                divSuper.classList.add('animate__animated', 'animate__bounceIn');
-            }else{
-
-                var divApu = document.getElementById("divEncargado");
-                divApu.style.display = 'none';
-                divApu.classList.add('animate__animated', 'animate__bounceIn');
-
-                var divSuper = document.getElementById("divApu");
-                divSuper.style.display = 'block';
-                divSuper.classList.add('animate__animated', 'animate__bounceIn');
-            }
-        });
-    }
-
-    $(document).ready(function () {
-        $('#nextButton').click(function () {
-            $('#modal1').modal('hide');
-        });
-
-        $('#modal1').on('hidden.bs.modal', function () {
-            $('#modal2').modal('show');
-        });
-    });
-
-
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-
-    gtag('js', new Date());
-    gtag('config', 'UA-56159088-1');
-
-    llenarAPU();
-
-    function llenarAPU() {
-        $.getJSON('https://grammermx.com/RH/Cursos/dao/daoApu.php', function (data) {
-            var select = document.getElementById("cbApu");
-            for (var i = 0; i < data.data.length; i++) {
-                var createOption = document.createElement("option");
-                createOption.text = data.data[i].Nombre;
-                createOption.value = data.data[i].Puesto;
-                select.appendChild(createOption);
-            }
-        });
-    }
-
-    llenarEncargado();
-
-    function llenarEncargado() {
-        $.getJSON('https://grammermx.com/RH/Cursos/dao/daoEncargado.php', function (data) {
-            var select = document.getElementById("cbEncargado");
-            for (var i = 0; i < data.data.length; i++) {
-                var createOption = document.createElement("option");
-                createOption.text = data.data[i].nombre;
-                createOption.value = data.data[i].nombre;
-                select.appendChild(createOption);
-            }
-        });
-    }
-
-    function llenarSuper() {
-        $.getJSON('https://grammermx.com/RH/Cursos/dao/daoSupervisor.php?APU=' + document.getElementById("cbApu").value, function (data) {
-            var selectS = document.getElementById("cbSupervisor");
-            selectS.innerHTML = "";
-
-            var selectA = document.getElementById("cbShiftleader");
-            selectA.innerHTML = "";
-
-
-            var createOptionDef = document.createElement("option");
-            createOptionDef.text = "Seleccione";
-            createOptionDef.value = "";
-            selectS.appendChild(createOptionDef);
-
-            for (var i = 0; i < data.data.length; i++) {
-                var createOptionS = document.createElement("option");
-                createOptionS.text = data.data[i].Supervisor;
-                createOptionS.value = data.data[i].Supervisor;
-                selectS.appendChild(createOptionS);
-            }
-
-            var divSuper = document.getElementById("divSupervisor");
-            divSuper.style.display = 'block';
-            divSuper.classList.add('animate__animated', 'animate__bounceIn');
-
-        });
-    }
-
-    function llenarShift() {
-        $.getJSON('https://grammermx.com/RH/Cursos/dao/daoShiftLeader.php?APU=' + document.getElementById("cbSupervisor").value, function (data) {
-            var selectS = document.getElementById("cbShiftleader");
-            selectS.innerHTML = "";
-
-            var createOptionDefS = document.createElement("option");
-            createOptionDefS.text = "Seleccione";
-            createOptionDefS.value = "";
-            selectS.appendChild(createOptionDefS);
-
-            for (var i = 0; i < data.data.length; i++) {
-                var createOptionS = document.createElement("option");
-                createOptionS.text = data.data[i].ShiftLeader;
-                createOptionS.value = data.data[i].ShiftLeader;
-                selectS.appendChild(createOptionS);
-            }
-
-            var divShift = document.getElementById("divShiftleader");
-            divShift.style.display = 'block';
-            divShift.classList.add('animate__animated', 'animate__bounceIn');
-
-        });
-    }
-
-    var cursos = [];
-
-    function cargarCursosSelect() {
-
-        $.getJSON('https://grammermx.com/RH/Cursos/dao/consultaCursoCapacidad.php', function (data) {
-            cursos = data.data;
-            var select = document.getElementById("cbCurso");
-            select.innerHTML = "";
-            var nombresCursos = [];
-
-            var createOption = document.createElement("option");
-            createOption.text = "Seleccione curso";
-            createOption.value = "Seleccione curso";
-            select.appendChild(createOption);
-
-            for (var i = 0; i < cursos.length; i++) {
-                if (!nombresCursos.includes(cursos[i].NombreCurso)) {
-                        nombresCursos.push(cursos[i].NombreCurso);
-                        createOption = document.createElement("option");
-                        createOption.text = cursos[i].NombreCurso;
-                        createOption.value = cursos[i].NombreCurso;
-                        select.appendChild(createOption);
-                }
-            }
-            var divCurso = document.getElementById("divCurso");
-            divCurso.style.display = 'block';
-            divCurso.classList.add('animate__animated', 'animate__bounceIn');
-        });
-    }
-
-    function cargarFechasHorarios() {
-        var select = document.getElementById("cbFechaHora");
-        select.innerHTML = ""; // Limpiar el select
-        var cursoSeleccionado = document.getElementById("cbCurso").value;
-        for (var i = 0; i < cursos.length; i++) {
-            if (cursos[i].NombreCurso == cursoSeleccionado) {
-
-                if (parseInt(cursos[i].CapacidadRestante) <= 0){
-
-                }else{
-                    var createOption = document.createElement("option");
-                    createOption.text = cursos[i].Fecha + ", " + cursos[i].Horario + " (" + cursos[i].CapacidadRestante + " disponibles)";
-                    createOption.value = cursos[i].Fecha + ", " + cursos[i].Horario;
-                    select.appendChild(createOption);
-                }
-
-            }
-        }
-    }
-
-    function cargarSeleccionCursos() {
-        var select = document.getElementById("cbHorario");
-        var nuevoValor = document.getElementById("cbCurso").value + ", " + document.getElementById("cbFechaHora").value;
-
-        // Verificar si el valor ya existe en las opciones
-        for (var i = 0; i < select.options.length; i++) {
-            if (select.options[i].value == nuevoValor) {
-                // El valor ya existe, así que salimos de la función
-                return;
-            }
-        }
-
-        // Si llegamos aquí, el valor no existe en las opciones, así que lo agregamos
-        var createOption = document.createElement("option");
-        createOption.text = nuevoValor;
-        createOption.value = nuevoValor;
-        select.appendChild(createOption);
-    }
-
-    function eliminarCurso() {
-        var select = document.getElementById("cbHorario");
-        var selectedIndex = select.selectedIndex;
-
-        if (selectedIndex != -1) {
-            select.remove(selectedIndex);
-        }
-    }
-
-    async function agregarAsistenciaCurso() {
-        var selectedOption = document.querySelector('input[name="inlineRadioOptions"]:checked').value;
-        console.log(selectedOption);
-        var nomina = document.getElementById("txtNominaCurso");
-        var nombre = document.getElementById("txtNombrePersonaCurso");
-        var apu = document.getElementById("cbApu");
-        var supervisor = document.getElementById("cbSupervisor");
-        var shiftLeader = document.getElementById("cbShiftleader");
-        var cursos = document.getElementById("cbHorario");
-        var encargado = document.getElementById("cbEncargado");
-
-        var errorOcurrido = false;
-
-        const dataRegistro = new FormData();
-
-        dataRegistro.append('nomina', nomina.value.trim());
-        dataRegistro.append('nombre', nombre.value.trim());
-
-
-        if (selectedOption === "Administrativo"){
-            dataRegistro.append('apu', "NA");
-            dataRegistro.append('supervisor', encargado.value.trim());
-            dataRegistro.append('shiftLeader', "NA");
-        }else{
-            dataRegistro.append('apu', apu.value.trim());
-            dataRegistro.append('supervisor', supervisor.value.trim());
-            dataRegistro.append('shiftLeader', shiftLeader.value.trim());
-        }
-
-        var fetchPromises = [];
-
-        for (var i = 0; i < cursos.options.length; i++) {
-            dataRegistro.append('horario', cursos.options[i].value.trim());
-
-            var horarioNormal = cursos.options[i].value;
-            var horarioParts = horarioNormal.split(", ");
-
-            var Curso = horarioParts[0].trim();
-            var Fecha = horarioParts[1].trim();
-            var HorarioCurso = horarioParts[2].trim();
-
-            await $.getJSON('https://grammermx.com/RH/Cursos/dao/consultaCapacidad.php?curso=' + Curso + '&fecha=' + Fecha + '&horario=' + HorarioCurso, function (data) {
-                if (data.data[0].Contador < data.data[0].Capacidad) {
-                    var fetchPromise = fetch('dao/guardarBitacoraCurso.php', {
-                        method: 'POST',
-                        body: dataRegistro
-                    })
-                        .then(function (response) {
-                            if (!response.ok) {
-                                throw "Error en la llamada Ajax";
-                            }
-                        })
-                        .catch(function (err) {
-                            console.log(err);
-                        });
-
-                    fetchPromises.push(fetchPromise);
-                } else {
-                    errorOcurrido = true;
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Cupo lleno.', showConfirmButton: false, input: 'none',
-                        text: 'No se acompleto el registro debido a la capacidad del curso.'
-                    })
-                }
-            });
-
-        }
-
-        Promise.all(fetchPromises)
-            .then(function () {
-                if (!errorOcurrido) {
-                    return Swal.fire({
-                        icon: 'success',
-                        title: 'Enviado.',
-                        showConfirmButton: false,
-                        input: 'none',
-                        text: 'Ya esta inscrito en los o el curso.',
-                        timer: 1800
-                    });
-                }
-            })
-            .then(function() {
-                location.reload();
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-
-    }
-
     cargarHorarios();
-
     function cargarHorarios() {
-        $.getJSON('https://grammermx.com/RH/Cursos/dao/consultaCursoUsuario.php?nomina=' + document.getElementById("txtNominaCurso").value, function (data) {
-            var table = $("#tablaCursosPendientes"); // selecciona la tabla
-            var tableConcluidos = $("#tablaConcluidos"); // selecciona la tabla
-            var tableCertificados = $("#tablaCertificados"); // selecciona la tabla
-            var tablaCursosAsistidos = $("#tablaCursosAsistidos"); // selecciona la tabla
+        $.getJSON('https://grammermx.com/Trafico/Facturas/dao/consultaFacturasPendientes.php', function (data) {
+            var table = $("#tablaCursosPendientes");
             let fechaActual = new Date();
             fechaActual.setHours(0, 0, 0, 0);
 
@@ -476,74 +171,21 @@
 
                 if (data.data[i].EstatusAsistencia == '0' && fechaData >= fechaActual) {
                     var newRow = $("<tr></tr>");
-                    var nameCell = $("<td></td>").text(data.data[i].Curso); // asume que tienes un campo id en tus datos
-                    var horarioCell = $("<td></td>").text(data.data[i].Horario); // asume que tienes un campo id en tus datos
-                    var fechaCell = $("<td></td>").text(data.data[i].Fecha);
+                    var idCell = $("<td></td>").text(data.data[i].IdFactura);
+                    var folioCell = $("<td></td>").text(data.data[i].Folio);
+                    var documentoCell = $("<td></td>").text(data.data[i].Documento);
+                    var fechaCell = $("<td></td>").text(data.data[i].FechaRegistro);
+                    var usuarioCell = $("<td></td>").text(data.data[i].Usuario);
+                    var estatusCell = $("<td></td>").text(data.data[i].Estatus);
 
                     // agrega las celdas a la fila y la fila a la tabla
-                    newRow.append(nameCell);
+                    newRow.append(idCell);
+                    newRow.append(folioCell);
+                    newRow.append(documentoCell);
                     newRow.append(fechaCell);
-                    newRow.append(horarioCell);
+                    newRow.append(usuarioCell);
+                    newRow.append(estatusCell);
                     table.append(newRow);
-                }
-                if (data.data[i].EstatusAsistencia == '1' || data.data[i].EstatusAsistencia == '2' || data.data[i].EstatusAsistencia == '3') {
-                    var newRow = $("<tr></tr>");
-                    var nameCell = $("<td></td>").text(data.data[i].Curso); // asume que tienes un campo id en tus datos
-                    var horarioCell = $("<td></td>").text(data.data[i].Horario); // asume que tienes un campo id en tus datos
-                    var fechaCell = $("<td></td>").text(data.data[i].Fecha);
-
-                    // agrega las celdas a la fila y la fila a la tabla
-                    newRow.append(nameCell);
-                    newRow.append(fechaCell);
-                    newRow.append(horarioCell);
-                    tablaCursosAsistidos.append(newRow);
-                }
-                if (data.data[i].EstatusAsistencia == '2') {
-
-                    var newRowConcluido = $("<tr></tr>");
-                    var idCell = $("<td></td>").text(data.data[i].IdBitacoraCurso);
-                    var nameCursoCell = $("<td></td>").text(data.data[i].Curso); // asume que tienes un campo id en tus datos
-                    var horarioCursoCell = $("<td></td>").text(data.data[i].Horario); // asume que tienes un campo id en tus datos
-                    var fechaCursoCell = $("<td></td>").text(data.data[i].Fecha); // asume que tienes un campo id en tus datos
-                    if (data.data[i].Evaluacion === '0') {
-                        var evaluacionCell = $("<td></td>").html("<span class=\"badge badge-pill badge-warning text-white\">" + "Pendiente" + "</span>");
-                    } else {
-                        var evaluacionCell = $("<td></td>").text(data.data[i].Evaluacion);
-
-                    }
-                    var valorarCell = $("<td></td>").html(`<button type="button" class="btn mb-2 btn-secondary" onclick="llenarEvaluacion('${data.data[i].Curso}','${data.data[i].Horario}','${data.data[i].Fecha}','${data.data[i].IdBitacoraCurso}')" data-toggle="modal" data-target=".modal-full">Valorar</button>`);
-
-                    // agrega las celdas a la fila y la fila a la tabla
-                    newRowConcluido.append(idCell);
-                    newRowConcluido.append(nameCursoCell);
-                    newRowConcluido.append(horarioCursoCell);
-                    newRowConcluido.append(fechaCursoCell);
-                    newRowConcluido.append(evaluacionCell);
-                    newRowConcluido.append(valorarCell);
-
-                    tableConcluidos.append(newRowConcluido);
-                }
-                if (data.data[i].EstatusAsistencia == '3') {
-                    var newRowCertificado = $("<tr></tr>");
-                    var idCertificadoCell = $("<td></td>").text(data.data[i].IdBitacoraCurso);
-                    var nameCertificadoCell = $("<td></td>").text(data.data[i].Curso); // asume que tienes un campo id en tus datos
-                    var horarioCertificadoCell = $("<td></td>").text(data.data[i].Horario); // asume que tienes un campo id en tus datos
-                    var fechaCertificadoCell = $("<td></td>").text(data.data[i].Fecha); // asume que tienes un campo id en tus datos
-                    if (data.data[i].Evaluacion === '0') {
-                        var evaluacionCertificadoCell = $("<td></td>").html("<span class=\"badge badge-pill badge-warning text-white\">" + "Pendiente" + "</span>");
-                    } else {
-                        var evaluacionCertificadoCell = $("<td></td>").text(data.data[i].Evaluacion);
-
-                    }
-                    var certificadoCell = $("<td></td>").html(`<button type="button" class="btn mb-2 btn-secondary" onclick="generarCertificado('${data.data[i].Curso}','${data.data[i].Horario}','${data.data[i].Fecha}','${data.data[i].IdBitacoraCurso}')" >Descargar Certificado</button>`);
-
-                    newRowCertificado.append(idCertificadoCell);
-                    newRowCertificado.append(nameCertificadoCell);
-                    newRowCertificado.append(horarioCertificadoCell);
-                    newRowCertificado.append(fechaCertificadoCell);
-                    newRowCertificado.append(evaluacionCertificadoCell);
-                    newRowCertificado.append(certificadoCell);
-                    tableCertificados.append(newRowCertificado);
                 }
             }
         });
