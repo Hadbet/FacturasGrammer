c5d957c7fec0ba35e673825d3894c13c9d4c5547
hadbet <hadbet.altamirano@grammer.com>
2024-10-17 11:12:27 -0600

    CargaInicial

--- dao/consultaFacturasPendientesGenera.php
+++ dao/consultaFacturasPendientesGenera.php
@@ -11,9 +11,9 @@ function ContadorApu()
 
     $datos = mysqli_query($conex, "SELECT `IdFactura`, `Folio`, CONCAT('<a target=\"_blank\" class=\"btn btn-primary\" href=\"documentacion/', `Documento`, '.pdf\">', 'Ver documento', '</a>') AS `DocumentoLink`, `FechaRegistro`, `Usuario`, `Aprobacion`, `FolioWere`, `FechaAprobacion`, `Comentarios`,
 CASE 
-    WHEN `Estatus` = 0 THEN '<span class=\"badge\" style=\"background-color: yellow; color: black;\">Pendiente</span>'
-    WHEN `Estatus` = 1 THEN '<span class=\"badge\" style=\"background-color: green; color: black;\">Aprobado</span>'
-    WHEN `Estatus` = 2 THEN '<span class=\"badge\" style=\"background-color: red; color: black;\">Rechazado</span>'
+    WHEN `Estatus` = 0 THEN '<span class=\"badge\" style=\"background-color: yellow; color: white;\">Pendiente</span>'
+    WHEN `Estatus` = 1 THEN '<span class=\"badge\" style=\"background-color: green; color: white;\">Aprobado</span>'
+    WHEN `Estatus` = 2 THEN '<span class=\"badge\" style=\"background-color: red; color: white;\">Rechazado</span>'
 END AS `Estatus`
 FROM `Facturas`");
 
